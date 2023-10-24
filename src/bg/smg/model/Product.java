f37be860a91044f7fad711c32c7a4aca2b39df8b
MagiV-inf <m.vigenina@gmail.com>
2023-10-24 15:46:24 +0300

    First update SQL scripts Product java class pomo6

--- src/bg/smg/model/Product.java
+++ src/bg/smg/model/Product.java
@@ -1,4 +1,11 @@
 package bg.smg.model;
 
 public class Product {
+    private int id;
+    private String name;
+    private String description;
+    private Category category;
+    private double unit_price;
+    private Timestamp created;
+    private boolean is_active;
 }
